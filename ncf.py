741ba05df25c4fd9a3ca027bc73bc4c8a7b2722a
Paul Magron <paul.magron@irit.fr>
2021-05-18 15:24:39 +0200

    up

--- ncf.py
+++ ncf.py
@@ -64,7 +64,81 @@ class ModelMLP(Module):
         return pred_rat, w_mlp, h_mlp
 
 
-def train_ncf(params, neg_ratio=5):
+def train_ncf(params):
+
+    # Get the hyperparameters
+    lW, lH = params['lW'], params['lH']
+
+    # Get the number of songs and users
+    n_users = len(open(params['data_dir'] + 'unique_uid.txt').readlines())
+    n_songs_train = len(open(params['data_dir'] + 'unique_sid.txt').readlines())
+
+    # Path for the TP training data, features and the WMF
+    path_tp_train = params['data_dir'] + 'train_tp.num.csv'
+    path_features = params['data_dir'] + 'feats.num.csv'
+
+    # Get the playcount data, confidence, and precompute its transpose
+    train_data, _, _, conf = load_tp_data(path_tp_train, shape=(n_users, n_songs_train))
+
+    # Define and initialize the model, and get the hyperparameters
+    my_model = ModelMLP(n_users, n_songs_train, params['n_embeddings'])
+    my_model.requires_grad_(True)
+    my_model.to(params['device'])
+
+    # Training setup
+    my_optimizer = Adam(params=my_model.parameters(), lr=params['lr'])
+    torch.autograd.set_detect_anomaly(True)
+
+    # Define the dataset
+    my_dataset = DatasetAttributesRatings(features_path=path_features, tp_path=path_tp_train, n_users=n_users)
+    my_dataloader = DataLoader(my_dataset, params['batch_size'], shuffle=True, drop_last=True)
+
+    # Loop over epochs
+    u_total = torch.arange(0, n_users, dtype=torch.long).to(params['device'])
+    time_tot, loss_tot, val_ndcg_tot = 0, [], []
+    time_opt, ndcg_opt = time_tot, 0
+    my_model.train()
+    for ep in range(params['n_epochs']):
+        print('\nEpoch {e_:4d}/{e:4d}'.format(e_=ep + 1, e=params['n_epochs']), flush=True)
+        start_time_ep = time.time()
+        epoch_losses = []
+        for data in tqdm(my_dataloader, desc='Training', unit=' Batches(s)'):
+            my_optimizer.zero_grad()
+            # Load the user / item indices and (positive) counts
+            counts_tot = torch.transpose(data[1], 1, 0).to(params['device'])
+            it_batch = data[2].to(params['device'])
+            # Forward pass
+            pred_rat, w_mlp, h_mlp = my_model(u_total, None, it_batch)
+            # Back-propagation
+            loss = wpe_joint_ncf(counts_tot, pred_rat, w_mlp, h_mlp, lW, lH)
+            loss.backward()
+            clip_grad_norm_(my_model.parameters(), max_norm=1.)
+            my_optimizer.step()
+            epoch_losses.append(loss.item())
+
+        # Overall stats for one epoch
+        loss_ep = np.mean(epoch_losses)
+        loss_tot.append(loss_ep)
+        time_ep = time.time() - start_time_ep
+        time_tot += time_ep
+        val_ndcg = evaluate_uni(params, my_model, in_out='in', split='val')
+        val_ndcg_tot.append(val_ndcg)
+        print('\nLoss: {l:6.6f} | Time: {t:5.3f} | NDCG: {n:5.3f}'.format(l=loss_ep, t=time_ep, n=val_ndcg),
+              flush=True)
+
+        # Save the model if it performs the best
+        if val_ndcg > ndcg_opt:
+            ndcg_opt = val_ndcg
+            time_opt = time_tot
+            torch.save(my_model.state_dict(), os.path.join(params['out_dir'], 'model.pt'))
+
+    # Record the training log
+    np.savez(os.path.join(params['out_dir'], 'training.npz'), loss=loss_tot, time=time_opt, val_ndcg=val_ndcg_tot)
+
+    return
+
+
+def train_ncf_negsamp(params, neg_ratio=5):
 
     # Get the hyperparameters
     lW, lH = params['lW'], params['lH']
@@ -91,13 +165,12 @@ def train_ncf(params, neg_ratio=5):
     torch.autograd.set_detect_anomaly(True)
 
     # Define the dataset
-    #my_dataset = DatasetAttributesRatings(features_path=path_features, tp_path=path_tp_train, n_users=n_users)
     my_dataset = DatasetAttributesNegsamp(features_path=path_features, tp_path=path_tp_train,
                                           tp_neg=path_tp_train_neg, n_users=n_users, n_songs=n_songs_train)
     my_dataloader = DataLoader(my_dataset, params['batch_size'], shuffle=True, drop_last=True)
 
     # Predefine the list of index to select after forward pass
-    idx_neg, idx_pos = torch.arange(1,neg_ratio), torch.tensor([0])
+    idx_neg, idx_pos = torch.arange(1, neg_ratio), torch.tensor([0])
     idx_neg_tot, idx_pos_tot = idx_neg, idx_pos
     for nn in range(params['batch_size'] - 1):
         idx_neg = idx_neg + (params['batch_size'] + 1) * neg_ratio
@@ -194,7 +267,8 @@ if __name__ == '__main__':
 
     data_dir = 'data/'
     # Training and validation for the hyperparameters
-    range_lW, range_lH = [0.01, 0.1, 1, 10], [0.01, 0.1, 1, 10]
+    #range_lW, range_lH = [0.01, 0.1, 1, 10], [0.01, 0.1, 1, 10]
+    range_lW, range_lH = [0], [0]
     train_main_ncf(params, range_lW, range_lH, data_dir)
 
 # EOF
