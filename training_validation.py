deb4c24f69ee187a8f05dc4f411d3755d3dc48c8
Paul Magron <paul.magron@inria.fr>
2022-06-27 15:21:04 +0200

    up

--- training_validation.py
+++ training_validation.py
@@ -68,6 +68,7 @@ if __name__ == '__main__':
             #range_inter, range_nl_di = ['mult', 'conc'], [-1, 0, 1, 2, 3, 4]
             range_inter, range_nl_di = ['conc'], [2, 3, 4]
             train_val_ncf(params, range_lW, range_lH, range_inter, range_nl_di, data_dir)
+            range_inter, range_nl_di = ['mult', 'conc'], [-1, 0, 1, 2, 3, 4]
             get_optimal_ncf(range_inter, range_nl_di)
 
         # NCACF - training with validation (interaction model, number of layers, variant)
