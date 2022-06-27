e929892217254a0c18e9b97d2f746aecab258b5e
Paul Magron <paul.magron@inria.fr>
2022-06-27 21:33:01 +0200

    up

--- training_validation.py
+++ training_validation.py
@@ -65,7 +65,8 @@ if __name__ == '__main__':
         elif model == 'ncf':
             range_lW, range_lH, = [0.1], [0.1]
             params['n_epochs'] = 100
-            range_inter, range_nl_di = ['mult', 'conc'], [-1, 0, 1, 2, 3, 4]
+            #range_inter, range_nl_di = ['mult', 'conc'], [-1, 0, 1, 2, 3, 4]
+            range_inter, range_nl_di = ['conc'], [4]
             train_val_ncf(params, range_lW, range_lH, range_inter, range_nl_di, data_dir)
             get_optimal_ncf(range_inter, range_nl_di)
 
