120263f2a23fa298af543422ed1307453aaa1979
Paul Magron <paul.magron@inria.fr>
2022-06-30 16:17:36 +0200

    up

--- training_validation.py
+++ training_validation.py
@@ -90,17 +90,17 @@ if __name__ == '__main__':
             #range_lW, range_lH, = [1], [0]
             #train_val_ncacf(setting_list, variant_list, params, range_lW, range_lH, range_inter, range_nl_di, data_dir)
 
-            setting_list = ['warm']
-            variant_list = ['strict']
-            range_inter = ['mult']
-            range_lW, range_lH, = [0.1], [0]
-            train_val_ncacf(setting_list, variant_list, params, range_lW, range_lH, range_inter, range_nl_di, data_dir)
-
             #setting_list = ['warm']
             #variant_list = ['strict']
-            #range_inter = ['conc']
+            #range_inter = ['mult']
             #range_lW, range_lH, = [0.1], [0]
             #train_val_ncacf(setting_list, variant_list, params, range_lW, range_lH, range_inter, range_nl_di, data_dir)
+
+            setting_list = ['warm']
+            variant_list = ['strict']
+            range_inter = ['conc']
+            range_lW, range_lH, = [0.1], [0]
+            train_val_ncacf(setting_list, variant_list, params, range_lW, range_lH, range_inter, range_nl_di, data_dir)
 
         else:
             print('Unknown model')
