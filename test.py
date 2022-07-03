df529c3b93f3b59029efe8fb0b9023333e794e5e
Paul Magron <paul.magron@inria.fr>
2022-07-03 12:03:52 +0200

    up

--- test.py
+++ test.py
@@ -181,6 +181,7 @@ if __name__ == '__main__':
 
     # Run on GPU (if it's available)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cpu'
     print('Process on: {}'.format(device))
     create_folder('outputs/temp/')
 
